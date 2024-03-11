ff80411dbf2a5f741fbbf1da834e94bf19bcbef3
famel marion <famelzuloaga@gmail.com>
2024-03-11 18:31:47 -0500

    query selector syntax changed

--- src/index.js
+++ src/index.js
@@ -37,31 +37,30 @@ textField.addEventListener("input", () => {
   // --- SETEA RESULTADOS EN ELEMENTOS HTML PARA MOSTRAR ----
   
   // 1. Setea conteo de palabras//15555
-  const numberofWords = document.querySelector("numberofWords");
+  const numberofWords = document.querySelector('li[data-name="numberofWords"]');
   numberofWords.textContent = "Number of words:" + numofWords;
 
   // 2. Setea conteo de caracteres
-  const numbfCharacters = document.querySelector("numofCharacters");
+  const numbfCharacters = document.querySelector('li[data-name="numofCharacters"]');
   numbfCharacters.textContent = "Number of Characters: " + numofCharacters;
   
   // 3. SETEA CONTEO DE CARACTERES SIN ESPACIO
-
-  const numbfCharactersExcludingSpaces = document.querySelector("numofCharactersExcludingSpaces");
+  const numbfCharactersExcludingSpaces = document.querySelector('li[data-name="numofCharactersExcludingSpaces"]');
   numbfCharactersExcludingSpaces.textContent = "Number of Characters Excluding Spaces : " + numofCharactersExcludingSpace;
 
   // 4. SETEA GETAVERAGE
 
-  const numberofaverageWordLength = document.querySelector("numberofaverageWordlength");
+  const numberofaverageWordLength = document.querySelector('li[data-name="numberofaverageWordlength"]');
   numberofaverageWordLength.textContent = "Average Word Length :" + averageWordLength;
 
   //5. setea numbercount
 
-  const numberofnumbercount = document.querySelector("numberofnumbercount");
+  const numberofnumbercount = document.querySelector('li[data-name="numberofnumbercount"]');
   numberofnumbercount.textContent = "Number Count :" + numberCount;
 
 
   // SETEA NUMBERSUM
-  const numberofnumberSum = document.querySelector("numberofnumbersum");
+  const numberofnumberSum = document.querySelector('li[data-name="numberofnumbersum"]');
   numberofnumberSum.textContent = "Number Sum :" + numberSum;
 
 })
