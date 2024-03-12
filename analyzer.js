19ccd60881c539cbdab553f08ab9a3be1223dfeb
famel marion <famelzuloaga@gmail.com>
2024-03-12 09:36:56 -0500

    Updates

---
+++ analyzer.js
@@ -0,0 +1,145 @@
+
+const analyzer = {
+  getWordCount: (text) => {
+    text = text.trim();
+    const words = text.split(" ");
+    const filteredWords = words.filter(word => word !== '');
+    return filteredWords.length;
+
+  },
+
+  // TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  getCharacterCount: (text) => {
+    // Obtener el contenido del textarea
+    //  const text = document.getElementById('textInput').value;
+    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    let contador = 0;
+    for (let i = 0; i < text.length; i++) {
+      contador += 1
+    }
+    return contador;
+  },
+
+
+  getCharacterCountExcludingSpaces: (text) => {
+    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+
+    let contador = 0;
+    let code;
+    for (let i = 0; i < text.length; i++) {
+      code = text.charCodeAt(i);
+      if ((code > 47 && code < 58) ||// numeric (0-9)
+        (code > 64 && code < 91) || // upper alpha (A-Z)
+        (code > 96 && code < 123)) { // lower alpha (a-z)
+        contador += 1
+      }
+    }
+
+    return contador;
+  },
+  getAverageWordLength: (text) => {
+    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const txt = text.trim(); // trim remueve espacios del inicio y final del texto
+    const words = txt.split(" "); // divide usando los espacios, crea un array palabras
+
+    let longs = 0;   // guarda longitud de cada palabra total
+    // va palabra por palabra y suma las logitudes de cada una
+    words.forEach(function (word) {
+      longs = longs + word.length
+
+    })
+    // promedio de longitudes
+    return Number((longs / words.length).toFixed(2)); // solo 2 decimales
+  },
+
+  getNumberCount: (text) => {
+    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    // 
+    // usa bucle for para ir letra por letra
+    const txt = text.trim(); // trim remueve espacios del inicio y final del texto
+    const words = txt.split(" "); //
+
+    let count = 0; // usado para contar caracteres numericos
+
+    words.forEach(function (word) { // va tras cada caracter
+      // Check if the character is a digit
+      if (word.match(/^-?\d+$/) || word.match(/^\d+\.\d+$/)) { // regex expresion para comparacion estricta para numero enteros y decimales(limitante)
+        // no hace nada
+        // console.log("--", word)
+        count++;
+      }
+    })
+
+    return count;
+  },
+
+  getNumberSum: (text) => {
+
+
+    function containsOnlyNumbers(word) {
+      for (let i = 0; i < word.length; i++) {
+        const code = word.charCodeAt(i);
+        if ((code > 47 && code < 58) || (code === 46)) {
+          continue
+        } else {
+          return false;
+        }
+      } containsOnlyNumbers
+      return true
+    }
+
+    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    let count = 0; // usado para contar caracteres numericos
+    const txt = text.trim(); // trim remueve espacios del inicio y final del texto
+    const words = txt.split(" "); // crea arreglo de palabras
+
+
+    for (let i = 0; i < words.length; i++) { // va tras cada caracter
+      // Check if the character is a digit
+      if (!isNaN(parseFloat(words[i])) && containsOnlyNumbers(words[i])) { // !isNaN verifica si es un numero valido
+        // console.log("is number", words[i])
+        count += parseFloat(words[i])
+      }
+    }
+    return count;
+    // --------- set de caracteres permitidos para un numero ----- 
+    //const set =  {} 
+    //set["1"] = true
+    //set["2"] = true
+    //set["3"] = true
+    //set["4"] = true
+    //set["5"] = true
+    //set["6"] = true
+    //set["7"] = true
+    //set["8"] = true
+    //set["9"] = true
+    //set["0"] = true
+    //set["."] = true
+
+    //function isNumber(word) {
+    // para cada letra en una palabra
+    //for(const char of word) {   
+    // si la palabra no se halla en el set, retorna falso
+    //if (!set[char]) return false
+    //}//
+    //return true
+    //} 
+
+    //const txt = text.trim(); // trim remueve espacios del inicio y final del texto
+    //const words = txt.split(" "); // crea arreglo de palabras
+
+    //let count = 0;  // acumula la suma de numeros 
+
+    // bucle foreach para recorrer cada palabra
+    //words.forEach(function (word) { 
+    //if (isNumber(word)) {
+    //count += parseFloat(word)
+    //}
+    //})
+
+    //return count;
+  }
+}
+//document.getElementById("myForm").reset();  
+
+export default analyzer;
---
+++ index.js
@@ -0,0 +1,73 @@
+import analyzer from './analyzer.js';
+
+//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+const textField = document.querySelector(`textarea[name ="user-input"]`);
+textField.addEventListener("input", () => {
+
+  // CALCULA USANDO LAS FUNCIONES
+
+  // obten texto de entrada
+  //const text = document.getElementById("textInput").value;
+  // console.log("txt: ", text)
+
+  // 1. CONTEO DE PALABRAS
+  const numofWords = analyzer.getWordCount(textField.value);
+  // console.log("--", numofWords)
+  
+  // 2. Conteo de caracteres
+  const numofCharacters = analyzer.getCharacterCount(textField.value);
+  // console.log("numofCharacters", numofCharacters)
+
+  // 3. ....getCharacterCountExcludingSpaces
+  const numofCharactersExcludingSpace = analyzer.getCharacterCountExcludingSpaces(textField.value);
+  // console.log("numofCharacters", numofCharactersExcludingSpace)
+
+  // 4 ... getAverageWordLength
+  const averageWordLength = analyzer.getAverageWordLength(textField.value);
+  // console.log("numofaverageWordLength", averageWordLength)
+
+  // 5... get number count
+  const numberCount = analyzer.getNumberCount(textField.value);
+  // console.log("numofnumberCount", numberCount)
+
+  // getnumberSum
+  const numberSum = analyzer.getNumberSum(textField.value);
+  // console.log("numofnumberSum", numberSum)
+
+  // --- SETEA RESULTADOS EN ELEMENTOS HTML PARA MOSTRAR ----
+  
+  // 1. Setea conteo de palabras//15555
+  const numberofWords = document.querySelector('li[data-name="numberofWords"]');
+  numberofWords.textContent = "Number of words:" + numofWords;
+
+  // 2. Setea conteo de caracteres
+  const numbfCharacters = document.querySelector('li[data-name="numofCharacters"]');
+  numbfCharacters.textContent = "Number of Characters: " + numofCharacters;
+  
+  // 3. SETEA CONTEO DE CARACTERES SIN ESPACIO
+  const numbfCharactersExcludingSpaces = document.querySelector('li[data-name="numofCharactersExcludingSpaces"]');
+  numbfCharactersExcludingSpaces.textContent = "Number of Characters Excluding Spaces : " + numofCharactersExcludingSpace;
+
+  // 4. SETEA GETAVERAGE
+
+  const numberofaverageWordLength = document.querySelector('li[data-name="numberofaverageWordlength"]');
+  numberofaverageWordLength.textContent = "Average Word Length :" + averageWordLength;
+
+  //5. setea numbercount
+
+  const numberofnumbercount = document.querySelector('li[data-name="numberofnumbercount"]');
+  numberofnumbercount.textContent = "Number Count :" + numberCount;
+
+
+  // SETEA NUMBERSUM
+  const numberofnumberSum = document.querySelector('li[data-name="numberofnumbersum"]');
+  numberofnumberSum.textContent = "Number Sum :" + numberSum;
+
+})
+
+function reset () {
+
+  textField.value = "";
+}
+
+document.getElementById("reset-button").addEventListener("click",reset,false)
